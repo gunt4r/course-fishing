b4c44bbb673dc730e759c31a4d04bf937d226c27
Vlad Prangati <vladprangati@gmail.com>
2025-11-17 02:48:15 +0200

    changed names of the variables db

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 import createNextIntlPlugin from 'next-intl/plugin';
 // Define the base Next.js configuration
 const baseConfig: NextConfig = {
-  output: 'standalone',
   eslint: {
     dirs: ['.'],
     ignoreDuringBuilds: true,
